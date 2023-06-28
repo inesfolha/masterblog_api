6b072e1c31e68d934c2e006e0a4e77c5ed28593c
inesfolha <ines.folha98@gmail.com>
2023-06-28 01:03:22 +0100

    third commit

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -36,7 +36,7 @@ def add_posts():
     return jsonify(add_post), 201
 
 
-@app.route('/api/posts/<string:post_id>', methods=['DELETE'])
+@app.route('/api/posts/<string:post_id>', methods=['DELETE'])                       #WORKING IN POSTMAN FINE BUT NOT WORKING IN THE WEBSITE, BUTTON DOES NOTHING
 def delete(post_id):
     post_to_delete = None
     for post in POSTS:
@@ -50,6 +50,62 @@ def delete(post_id):
         return jsonify({"message": f"Post with id {post_id} has been deleted successfully."}), 200
 
     return jsonify({"error": "Post not found"}), 404
+
+
+@app.route('/api/posts/<string:post_id>', methods=['PUT'])
+def update_post(post_id):
+    for post in POSTS:
+        if post['id'] == post_id:
+            new_title = request.json.get('title', post['title'])
+            new_content = request.json.get('content', post['content'])
+
+            post['title'] = new_title
+            post['content'] = new_content
+
+            file_handler.save_file('posts.json', POSTS)
+
+            updated_post = {
+                "id": post_id,
+                "title": new_title,
+                "content": new_content
+            }
+            return jsonify(updated_post), 200
+
+    return jsonify({"error": "Post not found"}), 404
+
+
+@app.route('/api/posts/search', methods=['GET'])
+def search_posts():
+    title = request.args.get('title')
+    content = request.args.get('content')
+
+    if not title and not content:
+        return jsonify([])
+
+    matching_posts = []
+    for post in POSTS:
+        if title and title.lower() in post['title'].lower():
+            matching_posts.append(post)
+        elif content and content.lower() in post['content'].lower():
+            matching_posts.append(post)
+
+    return jsonify(matching_posts)
+
+
+@app.route('/api/posts', methods=['GET'])                   #NOT WORKING
+def sort_posts():
+    sort_by = request.args.get('sort')
+    direction = request.args.get('dir')
+
+    sorted_posts = POSTS
+
+    if sort_by is not None:
+        if direction and direction.lower() == 'desc':
+            sorted_posts = sorted(POSTS, key=lambda x: x[sort_by], reverse=True)
+        elif direction and direction.lower() == 'asc':
+            sorted_posts = sorted(POSTS, key=lambda x: x[sort_by], reverse=False)
+
+    return jsonify(sorted_posts)
 
 
 if __name__ == '__main__':
