cc19e0c5ff1988166f526fc1de37a37d93816e62
inesfolha <ines.folha98@gmail.com>
2023-06-30 17:56:29 +0100

    PEP8 structure changes

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -5,12 +5,16 @@ import file_handler
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
 
-POSTS = file_handler.load_file('posts.json')
-
 
 def id_generator():
-    if POSTS:
-        last_id = POSTS[-1]['id']
+    """Generates a new post ID based on the existing posts.
+
+    Returns:
+        int: The new post ID."""
+
+    posts = file_handler.load_file('posts.json')
+    if posts:
+        last_id = posts[-1]['id']
     else:
         last_id = 0
     new_id = last_id + 1
@@ -19,6 +23,12 @@ def id_generator():
 
 @app.route('/api/posts', methods=['POST'])
 def add_posts():
+    """Adds a new post.
+
+    Returns:
+        tuple: A tuple containing the JSON response and the HTTP status code."""
+
+    posts = file_handler.load_file('posts.json')
     title = request.json.get('title')
     content = request.json.get('content')
 
@@ -28,23 +38,32 @@ def add_posts():
 
     add_post = {'id': post_id, "title": title, "content": content}
 
-    POSTS.append(add_post)
-    file_handler.save_file('posts.json', POSTS)
+    posts.append(add_post)
+    file_handler.save_file('posts.json', posts)
 
     return jsonify(add_post), 201
 
 
 @app.route('/api/posts/<int:post_id>', methods=['DELETE'])
 def delete(post_id):
+    """Deletes a post with the specified post ID.
+
+    Args:
+        post_id (int): The ID of the post to delete.
+
+    Returns:
+        tuple: A tuple containing the JSON response and the HTTP status code."""
+
+    posts = file_handler.load_file('posts.json')
     post_to_delete = None
-    for post in POSTS:
+    for post in posts:
         if post['id'] == post_id:
             post_to_delete = post
             break
 
     if post_to_delete:
-        POSTS.remove(post_to_delete)
-        file_handler.save_file('posts.json', POSTS)
+        posts.remove(post_to_delete)
+        file_handler.save_file('posts.json', posts)
         return jsonify({"message": f"Post with id {post_id} has been deleted successfully."}), 200
 
     return jsonify({"error": "Post not found"}), 404
@@ -52,7 +71,17 @@ def delete(post_id):
 
 @app.route('/api/posts/<int:post_id>', methods=['PUT'])
 def update_post(post_id):
-    for post in POSTS:
+    """ Updates a post with the specified post ID.
+
+    Args:
+        post_id (int): The ID of the post to update.
+
+    Returns:
+        tuple: A tuple containing the JSON response and the HTTP status code.
+    """
+
+    posts = file_handler.load_file('posts.json')
+    for post in posts:
         if post['id'] == post_id:
             new_title = request.json.get('title', post['title'])
             new_content = request.json.get('content', post['content'])
@@ -60,7 +89,7 @@ def update_post(post_id):
             post['title'] = new_title
             post['content'] = new_content
 
-            file_handler.save_file('posts.json', POSTS)
+            file_handler.save_file('posts.json', posts)
 
             updated_post = {
                 "id": post_id,
@@ -74,6 +103,13 @@ def update_post(post_id):
 
 @app.route('/api/posts/search', methods=['GET'])
 def search_posts():
+    """ Searches for posts based on title and/or content.
+
+    Returns:
+        tuple: A tuple containing the JSON response and the HTTP status code.
+    """
+
+    posts = file_handler.load_file('posts.json')
     title = request.args.get('title')
     content = request.args.get('content')
 
@@ -81,7 +117,7 @@ def search_posts():
         return jsonify([])
 
     matching_posts = []
-    for post in POSTS:
+    for post in posts:
         if title and title.lower() in post['title'].lower():
             matching_posts.append(post)
         elif content and content.lower() in post['content'].lower():
@@ -92,8 +128,15 @@ def search_posts():
 
 @app.route('/api/posts', methods=['GET'])  # NOT WORKING
 def sort_posts():
+    """ Sorts posts based on a specified field and direction.
+
+    Returns:
+        tuple: A tuple containing the JSON response and the HTTP status code.
+    """
+
+    posts = file_handler.load_file('posts.json')
     sort_by = request.args.get('sort')
-    direction = request.args.get('dir')
+    direction = request.args.get('direction')
 
     valid_sort_fields = ['title', 'content']
     valid_sort_directions = ['asc', 'desc']
@@ -103,15 +146,15 @@ def sort_posts():
             return jsonify({"error": "Invalid sort field or direction"}), 400
 
         elif direction and direction.lower() == 'desc':
-            sorted_posts = sorted(POSTS, key=lambda x: x[sort_by], reverse=True)
+            sorted_posts = sorted(posts, key=lambda x: x[sort_by], reverse=True)
             return jsonify(sorted_posts)
 
         elif direction and direction.lower() == 'asc':
-            sorted_posts = sorted(POSTS, key=lambda x: x[sort_by], reverse=False)
+            sorted_posts = sorted(posts, key=lambda x: x[sort_by], reverse=False)
             return jsonify(sorted_posts)
 
     else:
-        return jsonify(POSTS)
+        return jsonify(posts)
 
 
 if __name__ == '__main__':
